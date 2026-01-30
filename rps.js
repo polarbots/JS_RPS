8f382eb8f252df57f274e9475754ee27823e877c
Mike <172455720+polarbots@users.noreply.github.com>
2026-01-29 21:59:21 -0500

    Added getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
-console.log("Hello!")
+function getComputerChoice() {
+    // Set compVal to a random number between 0 to 1
+    let compVal = Math.random();
+    console.log(compVal)
+
+    // If compVal is less than or equal to 0.33, return "rock"
+    // Else if compVal is greater than 0.33 and less than or equal to 0.66, return "paper"
+    // Else return "scissors"
+    if (compVal <= 0.33) {
+        return "rock"
+    } else if (compVal > 0.33 & compVal <= 0.66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+    
+}
